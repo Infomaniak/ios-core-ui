35afa4c968864f633af9563c19e3c7b628e5a822
Adrien Coye de Brunélis <adrien.coye@infomaniak.com>
2023-01-10 17:19:15 +0100

    feat: moved UI classes

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "InfomaniakCoreUI",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "InfomaniakCoreUI",
+            targets: ["InfomaniakCoreUI"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/immortal79/LocalizeKit", .upToNextMajor(from: "1.0.1")),
+        .package(url: "https://github.com/PhilippeWeidmann/SnackBar.swift", .upToNextMajor(from: "0.1.2"))
+    ],
+    targets: [
+        .target(
+            name: "InfomaniakCoreUI",
+            dependencies: [
+                "LocalizeKit",
+                .product(name: "SnackBar", package: "SnackBar.swift")
+            ],
+            resources: [
+                .copy("Resources")
+            ]),
+        .testTarget(
+            name: "InfomaniakCoreUITests",
+            dependencies: ["InfomaniakCoreUI"])
+    ]
+)
