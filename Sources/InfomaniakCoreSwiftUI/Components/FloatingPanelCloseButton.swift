a6eb6efaf7e026527ec5da0b34d27cf4bc42b55b
Valentin Perignon <valentin.perignon@infomaniak.com>
2025-01-29 11:39:49 +0100

    chore: Update imports

--- Sources/InfomaniakCoreSwiftUI/Components/FloatingPanelCloseButton.swift
+++ Sources/InfomaniakCoreSwiftUI/Components/FloatingPanelCloseButton.swift
@@ -16,7 +16,7 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import InfomaniakCoreCommonUI
+import DesignSystem
 import SwiftUI
 
 @available(iOS 15.0, *)
--- Sources/InfomaniakCoreSwiftUI/Modifiers/FloatingPanelHelper.swift
+++ Sources/InfomaniakCoreSwiftUI/Modifiers/FloatingPanelHelper.swift
@@ -16,8 +16,8 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import DesignSystem
 import InfomaniakCore
-import InfomaniakCoreCommonUI
 import InfomaniakDI
 import SwiftUI
 import SwiftUIBackports
