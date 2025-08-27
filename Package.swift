f7182c23b0cbec345ae2044afab63d9078468a3d
Matthieu Déglon <matthieu.deglon@infomaniak.com>
2025-08-27 14:58:22 +0200

    chore: Import lottie

--- Package.swift
+++ Package.swift
@@ -41,7 +41,8 @@ let package = Package(
         .package(url: "https://github.com/onevcat/Kingfisher", .upToNextMajor(from: "7.10.0")),
         .package(url: "https://github.com/matomo-org/matomo-sdk-ios", .upToNextMajor(from: "7.5.2")),
         .package(url: "https://github.com/shaps80/SwiftUIBackports", .upToNextMajor(from: "1.15.1")),
-        .package(url: "https://github.com/siteline/SwiftUI-Introspect", .upToNextMajor(from: "1.4.0-beta.1"))
+        .package(url: "https://github.com/siteline/SwiftUI-Introspect", .upToNextMajor(from: "1.4.0-beta.1")),
+        .package(url: "https://github.com/airbnb/lottie-spm", .upToNextMajor(from: "4.5.2"))
     ],
     targets: [
         .target(
@@ -64,6 +65,7 @@ let package = Package(
                 "DesignSystem",
                 "InfomaniakCoreUIResources",
                 "InfomaniakCoreCommonUI",
+                .product(name: "Lottie", package: "lottie-spm"),
                 .product(name: "InfomaniakCore", package: "ios-core"),
                 .product(name: "SwiftUIIntrospect-Static", package: "SwiftUI-Introspect")
             ]
